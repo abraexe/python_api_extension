10f09309a7e94716c2da4a9188a3b185e322a9b1
abra_giddings <abragiddings00@gmail.com>
2025-01-25 12:41:48 -0500

    sample hover, other minor changes to complmetion

--- test/src/extension.ts
+++ test/src/extension.ts
@@ -1,27 +1,43 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('test.extension', () => {
+	const popup = vscode.commands.registerCommand('test.extension', () => {
 		vscode.window.showInformationMessage('test running :3');
 	});
 	
-	const autofill = vscode.languages.registerCompletionItemProvider(
+	const completion = vscode.languages.registerCompletionItemProvider(
 		'python',
 		{
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 
 				const linePrefix = document.lineAt(position).text.slice(0, position.character);
-				if (!linePrefix.endsWith('cat.')) {
+				if (!linePrefix.endsWith('sample.')) {
 					return undefined;
 				}
 
 				return [
-					new vscode.CompletionItem('meow', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('miau', vscode.CompletionItemKind.Method),
+					new vscode.CompletionItem('fill', vscode.CompletionItemKind.Method),
+					new vscode.CompletionItem('complete', vscode.CompletionItemKind.Method),
 				];
 			}
 		},
 		'.'
 	);
-	context.subscriptions.push(disposable, autofill);
+	const hover = vscode.languages.registerHoverProvider(
+		'python', 
+		{
+			provideHover(document, position, token) {
+				const range = document.getWordRangeAtPosition(position);
+				const word = document.getText(range);
+				const line = document.lineAt(position).text;
+				if (word === "join" && line.search("os.path.")!==-1) {
+					return new vscode.Hover({
+						language: "python",
+						value: "hover"
+					});
+				}
+			}
+    	}
+	);
+	context.subscriptions.push(popup, completion);
 }
