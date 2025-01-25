e651c02c737ab25061b250d5a0127fd1a5efc2f1
Demeter6202 <dmb258@case.edu>
2025-01-25 11:17:07 -0500

    update .gitignore

--- test/src/extension.ts
+++ test/src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('test.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test!');
+		vscode.window.showInformationMessage('Goodbye World from test!');
 	});
 
 	context.subscriptions.push(disposable);
