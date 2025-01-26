3912a3e7e5425682fa68094d085ed14b09ca0523
abra_giddings <abragiddings00@gmail.com>
2025-01-26 13:03:16 -0500

    renaming stuff from test

---
+++ test/src/blendev.ts
@@ -0,0 +1,236 @@
+import * as vscode from 'vscode';
+import { fileFetch } from './fileFetch';
+import { NodeDependenciesProvider } from './documentationTreeProvider';
+import * as fs from 'fs';
+import path from 'path';
+
+interface Dictionary {
+	[key: string]: string
+}
+
+interface Dictionary_Module {
+	[key: string]: Module
+}
+
+interface Layer {
+	module: Module;
+	children: Dictionary_Module;
+}
+
+interface Module {
+	name: string;
+	attributes: Dictionary;
+	s_attributes: Dictionary;
+	methods: Dictionary;
+	s_methods: Dictionary;
+	functions: Dictionary;
+};
+
+function parse_modules(dump: Object): Layer[] {
+	let parsed : Layer[] = [];
+	const reserved = [String('EXT_ATTRIBUTES'), String('EXT_STATIC_ATTRIBUTES'), String('EXT_METHODS'), 
+		String('EXT_STATIC_METHODS'), 'EXT_FUNCTIONS'];
+	const layers = ['types', 'ops', 'context', 'app', 'msgbus', 'utils', 'path', 'props', 'data'];
+	for(let j=0; j < Object.values(dump).length; j++)
+	{
+		let l : Layer = {
+			module : {
+				name: layers[j],
+				attributes: Object.values(dump)[j],
+				s_attributes: Object.values(dump)[j],
+				methods: Object.values(dump)[j],
+				s_methods: Object.values(dump)[j],
+				functions: Object.values(dump)[j],
+			},
+			children: {}
+		};
+		let arr : Dictionary_Module = {};
+		for(let i=0; i<Object.values(Object.values(dump)[j]).length; i++)
+		{
+			let key : string = Object.keys(Object.values(dump)[j])[i];
+	 		let value : Object = Object(Object.values(Object.values(dump)[j])[i]);
+	 		if(reserved.indexOf(key)===-1){
+				let m : Module = {
+					name: key,
+					attributes: Object.values(value)[0] as Dictionary,
+					s_attributes: Object.values(value)[1] as Dictionary,
+					methods: Object.values(value)[2] as Dictionary,
+					s_methods: Object.values(value)[3] as Dictionary,
+					functions: Object.values(value)[4] as Dictionary,
+				};
+				arr[m.name] = m;
+	 		}
+			else{
+				if(key==="EXT_ATTRIBUTES"&&value!==null)
+				{
+					l.module.attributes = Object((value)) as Dictionary;
+				}
+				else if(key==="EXT_STATIC_ATTRIBUTES")
+				{
+					l.module.s_attributes = Object((value)) as Dictionary;
+				}
+				else if(key==="EXT_METHODS")
+				{
+					l.module.methods = Object((value)) as Dictionary;
+				}
+				else if(key==="EXT_STATIC_METHODS")
+				{
+					l.module.s_methods = Object((value)) as Dictionary;
+				}
+				else if(key==="EXT_FUNCTIONS")
+				{
+					l.module.functions = Object((value)) as Dictionary;
+				}
+			}
+		}
+		l.children = arr;
+		parsed.push(l);
+	}
+	return parsed;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+	// // ===== TREE VIEW =======
+	// const rootPath = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+	// ? vscode.workspace.workspaceFolders[0].uri.fsPath
+	// : undefined;
+
+	// if (rootPath === undefined) {
+	//  	vscode.window.showErrorMessage("There is no open workspace to search. Open a folder to begin.");
+	// } 
+	// else {
+	//  	let treeProvider = new NodeDependenciesProvider(rootPath);
+	//  	vscode.window.registerTreeDataProvider(
+	//  		'nodeDependencies',
+	//  		treeProvider
+	//  	);
+	//  	context.subscriptions.push(vscode.window.createTreeView('nodeDependencies', {
+	//  		treeDataProvider: treeProvider
+	//  	}));
+	// }
+
+	
+	// ======= OPEN A FILE IN TEXT EDITOR =======
+	// context.subscriptions.push(vscode.commands.registerCommand('test.disassemble', async () => {
+	// 	// This function is a mess
+	// 	let result;
+	// 	await vscode.window.showOpenDialog(fileFetch.dialogOptions).then(m => result = m);
+	// 	if (result === undefined) {
+	// 		vscode.window.showErrorMessage("File not found!");
+	// 	}
+	// 	else {
+	// 		let uri: vscode.Uri = result;
+	// 		fileFetch.openFile(uri.toString().substring(7), context);
+	// 	}
+		
+	// }));
+	const popup = vscode.commands.registerCommand('blendev.go', () => {
+		vscode.window.showInformationMessage('blendev started successfully');
+	});
+
+	let raw_data : string = "";
+	try {
+		raw_data = fs.readFileSync(path.join(__dirname, '../../resources/docs.json'), 'utf8');
+	} catch (err) {
+		console.error(err);
+	}
+
+	let dump : Object = JSON.parse(raw_data);
+	let data : Layer[] = parse_modules(Object.values(Object.values(dump)[0]));
+	// comment statement for aaron
+	//console.log(data[0].children['ParticleSystem']);
+	
+	const completion = vscode.languages.registerCompletionItemProvider(
+		'python',
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+
+				const line = document.lineAt(position).text.slice(0, position.character);
+				let token: string = line.substring(line.lastIndexOf(" ")+1, position.character);
+				let arr: vscode.CompletionItem[] = [];
+
+				// to do : add some flags if found stuff since returning doesn't short circuit anymore,
+				// will make stuff faster
+				if (token.startsWith('bpy.')) {
+					if(token.endsWith('bpy.')){
+						data.forEach(function (child) {
+							arr.push(new vscode.CompletionItem(child.module.name, vscode.CompletionItemKind.Module));
+						});
+					}
+					else{
+						data.forEach(function (child) {
+						if(token.startsWith('bpy.' + child.module.name + ".")){
+							if(token.endsWith('bpy.' + child.module.name + ".")){
+								Object.keys(child.module.attributes).forEach(function (gchild) {
+									arr.push(new vscode.CompletionItem(gchild, vscode.CompletionItemKind.Property));
+								});
+								Object.keys(child.module.s_attributes).forEach(function (gchild) {
+									arr.push(new vscode.CompletionItem(gchild, vscode.CompletionItemKind.Property));
+								});
+								Object.keys(child.module.methods).forEach(function (gchild) {
+									arr.push(new vscode.CompletionItem(gchild, vscode.CompletionItemKind.Method));
+								});
+								Object.keys(child.module.s_methods).forEach(function (gchild) {
+									arr.push(new vscode.CompletionItem(gchild, vscode.CompletionItemKind.Method));
+								});
+								Object.keys(child.module.functions).forEach(function (gchild) {
+									arr.push(new vscode.CompletionItem(gchild, vscode.CompletionItemKind.Method));
+								});
+								Object.keys(child.children).forEach(function (gchild) {
+									arr.push(new vscode.CompletionItem(gchild, vscode.CompletionItemKind.Property));
+								});
+								return;
+							}
+							else{
+								Object.values(child.children).forEach(function (gchild) {
+									if(token.endsWith('bpy.' + child.module.name + "." + gchild.name + ".")){
+										Object.keys(gchild.attributes).forEach(function (ggchild) {
+											arr.push(new vscode.CompletionItem(ggchild, vscode.CompletionItemKind.Property));
+										});
+										Object.keys(gchild.s_attributes).forEach(function (ggchild) {
+											arr.push(new vscode.CompletionItem(ggchild, vscode.CompletionItemKind.Property));
+										});
+										Object.keys(gchild.methods).forEach(function (ggchild) {
+											arr.push(new vscode.CompletionItem(ggchild, vscode.CompletionItemKind.Method));
+										});
+										Object.keys(gchild.s_methods).forEach(function (ggchild) {
+											arr.push(new vscode.CompletionItem(ggchild, vscode.CompletionItemKind.Method));
+										});
+										Object.keys(gchild.functions).forEach(function (ggchild) {
+											arr.push(new vscode.CompletionItem(ggchild, vscode.CompletionItemKind.Method));
+										});
+									}
+								});
+								return;
+								
+							}
+						}
+						});
+					}
+					return arr;
+				}
+				return undefined;
+			}
+		},
+		'.'
+	);
+	const hover = vscode.languages.registerHoverProvider(
+		'python', 
+		{
+			provideHover(document, position, token) {
+				const range = document.getWordRangeAtPosition(position);
+				const word = document.getText(range);
+				const line = document.lineAt(position).text;
+				if (word === "join" && line.search("os.path.")!==-1) {
+					return new vscode.Hover({
+						language: "python",
+						value: "hover"
+					});
+				}
+			}
+    	}
+	);
+
+	context.subscriptions.push(popup, completion);
+}
